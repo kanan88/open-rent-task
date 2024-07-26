02558592b5d316116ff687671b8d579767df55f3
Kanan Farzali <kanan.farzali@gmail.com>
2024-07-26 02:55:16 +0200

    replacing directions in list with icons

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,16 +1,52 @@
 import styles from './list.module.css';
 
+import {
+  MdOutlineBedroomParent,
+  MdOutlineBathroom,
+  MdOutlinePersonAddAlt,
+  MdMyLocation,
+  MdOutlineLocalParking,
+  MdOutlineGrass,
+  MdOutlineCleaningServices,
+  MdLocationCity,
+} from 'react-icons/md';
+
 const List = () => {
   return (
     <ul className={styles.list}>
-      <li className={styles.list__item}>Bedrooms: 6</li>
-      <li className={styles.list__item}>Bathrooms: 2</li>
-      <li className={styles.list__item}>Max tenants: 1</li>
-      <li className={styles.list__item}>Location: Brighton</li>
-      <li className={styles.list__item}>Free parking</li>
-      <li className={styles.list__item}>Private garden</li>
-      <li className={styles.list__item}>A regular FREE cleaner</li>
-      <li className={styles.list__item}>Council Tax Band: D</li>
+      <li className={styles.list__item}>
+        <MdOutlineBedroomParent className={styles.list__icon} />
+        <span>Bedrooms: 6</span>
+      </li>
+      <li className={styles.list__item}>
+        <MdOutlineBathroom className={styles.list__icon} />
+        <span>Bathrooms: 2</span>
+      </li>
+      <li className={styles.list__item}>
+        <MdOutlinePersonAddAlt className={styles.list__icon} />
+
+        <span>Max tenants: 1</span>
+      </li>
+      <li className={styles.list__item}>
+        <MdMyLocation className={styles.list__icon} />
+        <span>Location: Brighton</span>
+      </li>
+      <li className={styles.list__item}>
+        <MdOutlineLocalParking className={styles.list__icon} />
+        <span>Free parking</span>
+      </li>
+      <li className={styles.list__item}>
+        <MdOutlineGrass className={styles.list__icon} />
+        <span>Private garden</span>
+      </li>
+      <li className={styles.list__item}>
+        <MdOutlineCleaningServices className={styles.list__icon} />
+        <span>A regular FREE cleaner</span>
+      </li>
+      <li className={styles.list__item}>
+        <MdLocationCity className={styles.list__icon} />
+        <span>Council Tax Band: D</span>
+      </li>
     </ul>
   );
 };
